d02e0bbb58f900ccab927caea2fb077ee42604e6
Bruno Fioreze <brunofioreze@pop-os.localdomain>
2023-05-07 08:11:15 -0300

    added core_number

--- parallelism.py
+++ parallelism.py
@@ -1,18 +1,25 @@
-import multiprocessing, requests as rq
+import multiprocessing, requests as rq, psutil
 from pprint import pprint
 
 URL = "https://pokeapi.co/api/v2/pokemon"
 
 
 def detail_by_id(number):
+    core_number = psutil.Process().cpu_num()
     r = rq.get(f"{URL}/{number}")
     data = r.json()
-    data = {"name": data["name"], "status_code": r.status_code, "id": number}
+    data = {
+        "name": data["name"],
+        "status_code": r.status_code,
+        "id": number,
+        "core_number": core_number,
+    }
+    pprint(data)
     return data
 
 
 if __name__ == "__main__":
 
     numbers = range(1, 151)
-    with multiprocessing.Pool(processes=4) as pool:
+    with multiprocessing.Pool(processes=12) as pool:
         results = pool.map(detail_by_id, numbers)
