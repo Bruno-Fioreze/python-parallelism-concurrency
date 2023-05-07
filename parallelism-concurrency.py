3cff317c2ae369d4f5fe50e7613b09031b956f5b
Bruno Fioreze <brunofioreze@pop-os.localdomain>
2023-05-07 08:05:04 -0300

    added last example

--- parallelism-concurrency.py
+++ parallelism-concurrency.py
@@ -1,21 +1,28 @@
-import multiprocessing
+import multiprocessing, requests as rq
 import threading
+from pprint import pprint
 
 
-def square(number):
-    return number**2
+URL = "https://pokeapi.co/api/v2/pokemon"
 
 
-def print_numbers():
-    for i in range(1, 6):
-        print(i)
+def detail_by_id(number):
+    r = rq.get(f"{URL}/{number}")
+    data = r.json()
+    data = {"name": data["name"], "status_code": r.status_code, "id": number}
+    return data
+
+
+def another_function():
+    print("another function")
 
 
 if __name__ == "__main__":
-    numbers = [1, 2, 3, 4]
+    numbers = range(1, 10)
     with multiprocessing.Pool(processes=4) as pool:
-        results = pool.map(square, numbers)
-    t = threading.Thread(target=print_numbers)
+        results = pool.map(detail_by_id, numbers)
+
+    t = threading.Thread(target=another_function)
     t.start()
-    print(results)
+    pprint(results)
     t.join()
